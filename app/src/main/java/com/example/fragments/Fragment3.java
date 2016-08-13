5b30ccabf98dffb8d07ebd26642dae38bc0aa9c8
zfrbzkurt <zbzkurt@gmail.com>
2016-08-13 15:58:23 +0300

    Connected with WebService in cloud

--- app/src/main/java/com/example/fragments/Fragment3.java
+++ app/src/main/java/com/example/fragments/Fragment3.java
@@ -2,6 +2,7 @@ package com.example.fragments;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -16,17 +17,40 @@ import com.example.adapters.MyListViewAdapter;
 import com.example.models.MoneyItem;
 import com.example.suat.financialasistant.R;
 
+import org.ksoap2.SoapEnvelope;
+import org.ksoap2.serialization.SoapObject;
+import org.ksoap2.serialization.SoapPrimitive;
+import org.ksoap2.serialization.SoapSerializationEnvelope;
+import org.ksoap2.transport.HttpTransportSE;
+import org.w3c.dom.CharacterData;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+
+import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 
 
 public class Fragment3 extends Fragment {
 
+    //
+    private static final String SOAP_ACTION="http://tempuri.org/GunlukVeriSorgu";
+    private static final String METHOD_NAME ="GunlukVeriSorgu";
+    private static final String NAMESPACE ="http://tempuri.org/";
+    private static final String URL="http://zaferbozkurtt.azurewebsites.net/WebService1.asmx?wsdl";
+    //
+
     ListView listView;
     Thread t1;
     Thread t2;
-    public static int babayaro;
-    public static int anangil;
+    public static int babayaro=0;
+    public static float anangil=0;
     List<MoneyItem> listMoneyItem;
     View v;
     @Override
@@ -38,6 +62,10 @@ public class Fragment3 extends Fragment {
         listView= (ListView) v.findViewById(R.id.money_list_view);
         final Activity a=getActivity();
 
+        //
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        //
 
 
         listMoneyItem=new ArrayList<>();
@@ -46,6 +74,9 @@ public class Fragment3 extends Fragment {
         MyListViewAdapter myListViewAdapter=new MyListViewAdapter(
                 v.getContext().getApplicationContext(), R.layout.custom_row, listMoneyItem);
         listView.setAdapter(myListViewAdapter);
+        listMoneyItem.get(0).setMoneyType("USD-EUR");
+        listMoneyItem.get(0).setMoneyLow(""+babayaro);
+        listMoneyItem.get(0).setMoneyHigh(""+anangil);
 
         t1=new Thread(){
             @Override
@@ -53,17 +84,54 @@ public class Fragment3 extends Fragment {
                 try{
                     while (true)
                     {
-                        Thread.sleep(2000);
+                        //
+                        SoapObject request = new SoapObject(NAMESPACE,METHOD_NAME);
+                        SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
+                        envelope.dotNet=true;
+                        envelope.setOutputSoapObject(request);
+
+                        HttpTransportSE androidHttpTransport = new HttpTransportSE(URL);
+                        androidHttpTransport.debug=true;
+                        try{
+                            androidHttpTransport.call(SOAP_ACTION,envelope);
+                            SoapPrimitive response = (SoapPrimitive) envelope.getResponse();
+
+                            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+                            DocumentBuilder db = dbf.newDocumentBuilder();
+                            InputSource is = new InputSource();
+                            is.setCharacterStream(new StringReader(response.toString()));
+                            Document doc = db.parse(is);
+
+                            NodeList nodes = doc.getElementsByTagName("Table");
+
+                            for (int i =0;i<nodes.getLength();i++)
+                            {
+                                Element element = (Element) nodes.item(i);
+                                NodeList nameTarih = element.getElementsByTagName("Tarih");
+                                Element line1 = (Element) nameTarih.item(0);
+                                NodeList nameIslem = element.getElementsByTagName("Doviz_Alis");
+                                Element line2 = (Element) nameIslem.item(0);
+                                babayaro=Integer.parseInt( getCharacterDataFromElement(line1));
+                                anangil=Float.parseFloat( getCharacterDataFromElement(line2));
+
+                            }
+                        }
+                        catch (Exception e1){
+                            System.out.println("cekExceptionCalisti");
+                        }
+                        //
+
                         a.runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
                                 listMoneyItem.get(0).setMoneyType("USD-EUR");
                                 listMoneyItem.get(0).setMoneyLow(""+babayaro);
                                 listMoneyItem.get(0).setMoneyHigh(""+anangil);
-                                babayaro= babayaro + 1;
-                                anangil= anangil+5;
+                                //babayaro= babayaro + 1;
+                               // anangil= anangil+5;
                             }
                         });
+                        Thread.sleep(1000);
                     }
 
                 }
@@ -78,7 +146,7 @@ public class Fragment3 extends Fragment {
                 try{
                     while (true)
                     {
-                        Thread.sleep(2000);
+
                         a.runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
@@ -87,6 +155,7 @@ public class Fragment3 extends Fragment {
                                 listView.setAdapter(myListViewAdapter);
                             }
                         });
+                        Thread.sleep(1000);
                     }
 
                 }
@@ -110,7 +179,14 @@ public class Fragment3 extends Fragment {
             return v;
         }
 
-
+    public static String getCharacterDataFromElement(Element e) {
+        Node child = e.getFirstChild();
+        if (child instanceof CharacterData) {
+            CharacterData cd = (CharacterData) child;
+            return cd.getData();
+        }
+        return "";
+    }
     @Override
     public void onDestroyView() {
         super.onDestroyView();
