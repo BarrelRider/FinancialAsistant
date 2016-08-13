529e907325aa9ac4eac738d480c4ce82ed3633e0
Suat Ogme <suat.ogme@gmail.com>
2016-08-13 14:53:16 +0300

    Updating exchange rates in every 2 sec.

--- app/src/main/java/com/example/fragments/Fragment3.java
+++ app/src/main/java/com/example/fragments/Fragment3.java
@@ -1,8 +1,10 @@
 package com.example.fragments;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,7 +23,10 @@ import java.util.List;
 public class Fragment3 extends Fragment {
 
     ListView listView;
-    public String veri;
+    Thread t1;
+    Thread t2;
+    public static int babayaro;
+    public static int anangil;
     List<MoneyItem> listMoneyItem;
     View v;
     @Override
@@ -31,29 +36,88 @@ public class Fragment3 extends Fragment {
 
         v=inflater.inflate(R.layout.fragment3_layout,container,false);
         listView= (ListView) v.findViewById(R.id.money_list_view);
+        final Activity a=getActivity();
+
+
 
         listMoneyItem=new ArrayList<>();
-        listMoneyItem.add(new MoneyItem("EUR-USD","1.111","1.123"));
-        listMoneyItem.add(new MoneyItem("GBP-USD","1.041","1.333"));
-        listMoneyItem.add(new MoneyItem("TRY-USD", "1.101", "2.834"));
-        listMoneyItem.add(new MoneyItem("CHF-USD", "1.134", "1.533"));
-
+        listMoneyItem.add(new MoneyItem("Bekle","Bekle","Bekle"));
 
         MyListViewAdapter myListViewAdapter=new MyListViewAdapter(
-                v.getContext().getApplicationContext(),R.layout.custom_row,listMoneyItem);
-
+                v.getContext().getApplicationContext(), R.layout.custom_row, listMoneyItem);
         listView.setAdapter(myListViewAdapter);
+
+        t1=new Thread(){
+            @Override
+            public void run() {
+                try{
+                    while (true)
+                    {
+                        Thread.sleep(2000);
+                        a.runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                listMoneyItem.get(0).setMoneyType("USD-EUR");
+                                listMoneyItem.get(0).setMoneyLow(""+babayaro);
+                                listMoneyItem.get(0).setMoneyHigh(""+anangil);
+                                babayaro= babayaro + 1;
+                                anangil= anangil+5;
+                            }
+                        });
+                    }
+
+                }
+                catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+        };
+         t2=new Thread(){
+            @Override
+            public void run() {
+                try{
+                    while (true)
+                    {
+                        Thread.sleep(2000);
+                        a.runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                MyListViewAdapter myListViewAdapter=new MyListViewAdapter(
+                                        v.getContext().getApplicationContext(), R.layout.custom_row, listMoneyItem);
+                                listView.setAdapter(myListViewAdapter);
+                            }
+                        });
+                    }
+
+                }
+                catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+        };
+
+
+        t1.start();
+        t2.start();
+
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id)
-                {
-                    Toast.makeText(getContext(), listMoneyItem.get(position).getMoneyType() + " Bastin", Toast.LENGTH_SHORT).show();
-                }
-            });
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getContext(), listMoneyItem.get(position).getMoneyType() + " Bastin", Toast.LENGTH_SHORT).show();
+            }
+        });
             return v;
         }
+
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        t1.interrupt();
+        t2.interrupt();
     }
+}
 
 
 
